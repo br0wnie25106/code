3e805500ed7b43d97ac0ce4d0561fe7e1db0910f
pitchaya <106579110+br0wnie25106@users.noreply.github.com>
2022-10-18 13:29:49 +0700

    Add files via upload

---
+++ structure04.cpp
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+main () {
+	int i,a,Maxy,Maxm,Maxd,y1,y2,x;
+  struct date {
+    int day;
+    int month;
+    int year;
+  };
+  struct student {
+    int std_id;
+    char std_name[40];
+    struct date std_birth;
+    struct date std_checkin;
+    int room;
+  };
+
+  typedef struct student STD;
+  STD stdrecord[5] = { {10001, "Chris Hemsworth" ,11, 12, 2552, 3 ,5 ,2560, 4} ,
+                       {10002, "Tom Cruise" ,3 ,4 ,2552 ,6 ,5 ,2555, 4},
+                       {10003, "Taylor Swift" ,13 ,12, 2552, 1 ,5, 2560, 2},
+                       {10004, "Hugh Grant", 9 ,9 ,2551, 1 ,5, 2560, 1},
+                       {10005, "Kristen Stewart", 9 ,4, 2552 ,7 ,5 ,2560, 1} };
+    
+    printf("Enter month of birth : ");
+    scanf("%d",&x);
+
+   for(i=0;i<5;i++) {
+   	if(stdrecord[i].std_birth.month==x) 
+   		printf("the student is %s\n",stdrecord[i].std_name);
+   }
+  
+}
